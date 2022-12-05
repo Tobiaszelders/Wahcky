59ab21ec9d1921f4d38a3c35318ab8e2d761054f
Tobias <Tobizelders@gmail.com>
2022-12-05 09:28:33 +0100

    Initial commit
    In this commit i've made a plane with a player that can move around. Next to the player is a NPC with a interactive sound.

---
+++ Wahcky (NPC's & Interaction)/Assets/C# Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameObject triggeringNPC;
+    private bool triggering;
+
+    public GameObject npcText;
+
+    private void Update()
+    {
+        if (triggering)
+        {
+            npcText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //How to activate a sound:
+                FindObjectOfType<AudioManager>().Play("npcTalk");
+
+                Debug.Log("Bla bla bla bla bla");
+            }
+        }
+        else
+        {
+            npcText.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "NPC")
+        {
+            triggering = true;
+            triggeringNPC = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "NPC")
+        {
+            triggering = false;
+            triggeringNPC = null;
+        }
+    }
+}
